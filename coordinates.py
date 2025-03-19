0dd0e63e988fef5fad7c25f0773225e012a2e1b6
Jacopo <jacopo.canton@gmail.com>
2025-03-19 18:47:24 +0100

    wed

--- coordinates.py
+++ coordinates.py
@@ -1,23 +1,51 @@
-# from sympy import symbols, sinh, exp, diff
+# SLEVE_NML
+# MIN_LAY_THCKN =    3.000000000000000     ,
+# MAX_LAY_THCKN =    25000.00000000000     ,
+# HTOP_THCKNLIMIT =    15000.00000000000     ,
+# TOP_HEIGHT =    600.0000000000000     ,
+# DECAY_SCALE_1 =    150.0000000000000     ,
+# DECAY_SCALE_2 =    150.0000000000000     ,
+# DECAY_EXP =    1.200000000000000     ,
+# FLAT_HEIGHT =    500.0000000000000     ,
+# STRETCH_FAC =    1.000000000000000     ,
+# ELSE IF (ivctype == 2 ) THEN ! SLEVE coordinate (Leuenberger et al. MWR 2010)
+# DO jk = 1, nflat
+#   jk1 = jk + nshift
+#   z3d_i(1:nlen,jk,jb) = vct_a(jk1)
+# ENDDO
+# DO jk = nflat + 1, nlev
+#   jk1 = jk + nshift
+#   ! Scaling factors for large-scale and small-scale topography
+#   z_fac1 = SINH((top_height/decay_scale_1)**decay_exp - (vct_a(jk1)/decay_scale_1)**decay_exp) / SINH((top_height/decay_scale_1)**decay_exp)
+#   z_fac2 = SINH((top_height/decay_scale_2)**decay_exp - (vct_a(jk1)/decay_scale_2)**decay_exp) / SINH((top_height/decay_scale_2)**decay_exp)
 #
-# # Define symbols
-# x, y, Z = symbols('x y Z')
-# hx, hy, hw, hh = symbols('hx hy hw hh')
-# s1, s2, H = symbols('s1 s2 H')
+#   ! Small-scale topography (i.e. full topo - smooth topo)
+#   z_topo_dev(1:nlen) = topo(1:nlen,jb) - topo_smt(1:nlen,jb)
 #
-# # Define functions
-# hill = hh * exp(- ((x-hx)**2 + (y-hy)**2)/hw**2)
-# h1 = hill
-# h2 = 0*x*y
-# b1 = sinh((H-Z)/s1) / sinh(H/s1)
-# b2 = sinh((H-Z)/s2) / sinh(H/s2)
-#
-# # Define z
-# z = Z + h1 * b1 + h2 * b2
-#
-# # Compute the derivative dz/dZ
-# dz_dZ = diff(z, Z)
-# print(dz_dZ)
+#   z3d_i(1:nlen,jk,jb) = vct_a(jk1) + topo_smt(1:nlen,jb)*z_fac1 + z_topo_dev(1:nlen)*z_fac2
+# ENDDO
+
+
+import sympy as sp
+
+# Define symbols
+x, y, Z = sp.symbols('x y Z')
+hx, hy, hw, hh = sp.symbols('hx hy hw hh')
+s1, s2, n, Zt = sp.symbols('s1 s2 H')
+
+# Define functions
+hill = hh * sp.exp( -( (x - hx)**2 + (y - hy)**2 ) / hw**2 )
+h1 = hill
+h2 = 0*x*y
+b1 = sp.sinh( (Zt/s1)**n - (Z/s1)**n ) / sp.sinh( (Zt/s1)**n )
+b2 = sp.sinh( (Zt/s2)**n - (Z/s2)**n ) / sp.sinh( (Zt/s2)**n )
+
+# Define z
+z = Z + h1 * b1 + h2 * b2
+
+# Compute the derivative dz/dZ
+dz_dZ = sp.diff(z, Z)
+print(dz_dZ)
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -32,33 +60,33 @@ hx = (x0+x1)/2; hy = (y0+y1)/2;
 
 # coordinates
 s1 = 150; s2 = 150;
-H = 500;
+Zt = 500;
 
 # functions
 hill = lambda x,y: hh * np.exp(- ((x-hx)**2 + (y-hy)**2)/hw**2)
 h1 = lambda x,y: hill(x,y);
 h2 = lambda x,y: 0*x*y;
-b1 = lambda Z: np.sinh((H-Z)/s1) / np.sinh(H/s1)
-b2 = lambda Z: np.sinh((H-Z)/s2) / np.sinh(H/s2)
+b1 = lambda Z: np.sinh((Zt-Z)/s1) / np.sinh(Zt/s1)
+b2 = lambda Z: np.sinh((Zt-Z)/s2) / np.sinh(Zt/s2)
 
 z = lambda x,y,Z:  Z + h1(x,y) * b1(Z)+ h2(x,y) * b2(Z);
 
 # plot
 px = np.linspace(x0,x1,100)
-pz = np.linspace(0,H, 100)
+pz = np.linspace(0,Zt, 100)
 plt.figure(1); plt.clf(); plt.show(block=False)
 #PX, PZ = np.meshgrid(px, pz)
 #plt.contourf(PX, PZ, z(PX,hy, PZ), 50, edgecolors='black')
 #plt.plot(px,hill(px,hy))
-for zhat in range(0,H,50):
+for zhat in range(0,Zt,50):
     plt.plot(px,z(px,hy, zhat))
 plt.draw()
 
 #-------------------------------------------------------------------------------
 # Jacobian
 #
-Jm1 = lambda x,y,Z: -hh*np.exp((-(-hx + x)**2 - (-hy + y)**2)/hw**2)*np.cosh((H - Z)/s1)/(s1*np.sinh(H/s1)) + 1
+Jm1 = lambda x,y,Z: -hh*np.exp((-(-hx + x)**2 - (-hy + y)**2)/hw**2)*np.cosh((Zt - Z)/s1)/(s1*np.sinh(Zt/s1)) + 1
 plt.figure(1); plt.clf(); plt.show(block=False)
-for zhat in range(0,H,50):
+for zhat in range(0,Zt,50):
     plt.plot(px,Jm1(px,hy, zhat))
 plt.draw()
--- ibm_artifacts.py
+++ ibm_artifacts.py
@@ -1,13 +1,16 @@
 import os
 import pickle
 from icon4py.model.common.states import prognostic_state
-import xarray as xr
 
 import gt4py.next as gtx
 import matplotlib.pyplot as plt
 import numpy as np
 
 from icon4py.model.common.io import plots
+
+# # fortran
+# ds = xr.open_dataset(f"/scratch/l_jcanton/plot_data/torus_exclaim.iccarus_hill/torus_exclaim_insta_DOM01_ML_0066.nc")
+# data  = ds.w.values[0,:,:].T
 
 # mpl.use("tkagg")
 
@@ -27,7 +30,7 @@ x = np.linspace(0, 1000, 500)
 y = 500
 hill = compute_hill_elevation(x, y)
 
-main_dir = os.getcwd() + "/"
+main_dir = os.getcwd() + "/../icon4py/"
 grid_file_path = "testdata/grids/gauss3d_torus/Torus_Triangles_1000m_x_1000m_res10m.nc"
 
 # -------------------------------------------------------------------------------
@@ -39,21 +42,30 @@ plot = plots.Plot(
     grid_file_path=main_dir + grid_file_path,
     backend=gtx.gtfn_cpu,
 )
+# [
+#   'theta_v_ic',
+#   'ddqz_z_half',
+#   'z_alpha',
+#   'z_beta',
+#   'z_w_expl',
+#   'z_exner_expl',
+#   'z_q'
+#   ]
 
-# # fortran
-# ds = xr.open_dataset(f"/scratch/l_jcanton/plot_data/torus_exclaim.iccarus_hill/torus_exclaim_insta_DOM01_ML_0066.nc")
-# data  = ds.w.values[0,:,:].T
 
+pickle_matr = main_dir + "imgs/00000_w_matrix.pkl"
+pickle_prog = main_dir + "imgs/00001_after_predictor_prognostic.pkl"
+pickle_diag = main_dir + "imgs/00002_after_predictor_diagnostic.pkl"
 
-pickle_prog = main_dir + "imgs/00000_after_predictor_prognostic.pkl"
-pickle_diag = main_dir + "imgs/00001_after_predictor_diagnostic.pkl"
-
+with open(pickle_matr, "rb") as f:
+    prognostic_matr= pickle.load(f)
 with open(pickle_prog, "rb") as f:
     prognostic_state = pickle.load(f)
 with open(pickle_diag, "rb") as f:
     diagnostic_state = pickle.load(f)
 
-data = prognostic_state.w
+vname = "z_q"
+data = prognostic_matr[vname]
 
 axs, x_coords_i, y_coords_i, u_i, w_i, idxs = plot.plot_sections(
     data=data,
@@ -71,4 +83,4 @@ axs[0].set_aspect("equal")
 axs[0].set_xlabel("x [m]")
 axs[0].set_ylabel("z [m]")
 plt.draw()
-#plt.savefig("w.png", bbox_inches="tight")
+plt.savefig(f"{vname}.png", bbox_inches="tight")
