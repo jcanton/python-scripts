0e0c4354bfc6d2c204ee95fd7e1cb11f1b5e3bac
Jacopo <jacopo.canton@gmail.com>
2025-07-10 15:32:33 +0200

    some new masked cells

--- ibm_masked_cells.py
+++ ibm_masked_cells.py
@@ -8,8 +8,11 @@ from icon4py.model.common.io import plots
 from icon4py.model.atmosphere.dycore import ibm
 
 main_dir = os.getcwd() + "/../icon4py/"
-grid_file_path = main_dir + "testdata/grids/gauss3d_torus/Torus_Triangles_1000m_x_1000m_res10m.nc"
-savepoint_path = "/scratch/mch/jcanton/ser_data/exclaim_gauss3d.uniform800_flat/ser_data/"
+#grid_file_path = main_dir + "testdata/grids/gauss3d_torus/Torus_Triangles_1000m_x_1000m_res10m.nc"
+#savepoint_path = main_dir + "ser_data/exclaim_gauss3d.uniform800_flat/ser_data/"
+
+grid_file_path = main_dir + "testdata/grids/gauss3d_torus/Torus_Triangles_250m_x_250m_res2.5m.nc"
+savepoint_path = main_dir + "ser_data/exclaim_gauss3d_250x250.uniform400_flat/ser_data/"
 
 plot = plots.Plot(
     savepoint_path=savepoint_path,
@@ -29,8 +32,10 @@ data = _ibm.full_cell_mask.asnumpy().astype(float)[:,-1]
 
 plt.figure(1); plt.clf(); plt.show(block=False)
 plt.tripcolor(plot.tri, data, edgecolor='black', shading='flat')
-plt.xticks(np.arange(0,1000,50))
-plt.yticks(np.arange(0,1000,50))
+#plt.xticks(np.arange(0,1000,50))
+#plt.yticks(np.arange(0,1000,50))
+plt.xticks(np.arange(0,250,50))
+plt.yticks(np.arange(0,250,50))
 plt.draw()
 plt.savefig("ibm_masked_cells.png", dpi=600)
 
