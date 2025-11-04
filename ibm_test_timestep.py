fe2c8b4e4e8da94243e9887c39ed9494736002a9
Jacopo <jacopo.canton@gmail.com>
2025-11-04 13:43:28 +0100

    update timestep test (wip)

--- ibm_test_timestep.py
+++ ibm_test_timestep.py
@@ -1,25 +1,6 @@
 import os
 import pickle
-
-# ------------------------------------------------------------------------------
-# mesh and levels
-#
-plotting_data_file = "plotting_channel_950x350x100_5m_nlev20.pkl"
-
-with open(os.path.join("data", plotting_data_file), "rb") as f:
-    plotting = pickle.load(f)
-    tri = plotting["tri"]
-    full_level_heights = plotting["full_level_heights"]
-    half_level_heights = plotting["half_level_heights"]
-    full_cell_mask = plotting["full_cell_mask"]
-    half_cell_mask = plotting["half_cell_mask"]
-    full_edge_mask = plotting["full_edge_mask"]
-    half_edge_mask = plotting["half_edge_mask"]
-full_levels = full_level_heights[0, :]
-half_levels = half_level_heights[0, :]
-
-num_cells = len(tri.cell_x)
-num_levels = len(full_levels)
+import xarray as xr
 
 # ------------------------------------------------------------------------------
 # handling variables
@@ -29,10 +10,15 @@ variables = ["vn", "w", "rho", "exner", "theta_v"]
 
 def load_state(fname, variables):
     state = {}
-    with open(fname, "rb") as ifile:
-        data = pickle.load(ifile)
+    if fname.endswith(".nc"):
+        ds = xr.open_dataset(fname)
         for variable in variables:
-            state[variable] = data[variable]
+            state[variable] = ds[variable].values[0,:,:].T
+    elif fname.endswith(".pkl"):
+        with open(fname, "rb") as ifile:
+            data = pickle.load(ifile)
+            for variable in variables:
+                state[variable] = data[variable]
     return state
 
 
@@ -41,8 +27,12 @@ def load_state(fname, variables):
 #
 # fname = os.path.join("data", "ibm_channel_reference_solution.pkl")
 fname = os.path.join(
-    "../runs/icon4py/test_channel_950m_x_350m_res5m_nlev20.reference",
-    "end_of_timestep_000000009.pkl",
+    "../icon4py/testdata/ser_icondata/mpitask1/test_channel_ibm/",
+    "end_of_timestep_000000000.pkl",
+    #"../icon-exclaim/build_cpu2py/experiments/exclaim_gauss3d_sb/",
+    #"torus_insta_DOM01_ML_0002.nc",
+    #"../icon-exclaim/build_cpu2py/experiments/exclaim_gauss3d_sb/undefined_output_runxxx/",
+    #"initial_condition_ibm_test.pkl"
 )
 ref_state = load_state(fname, variables)
 
@@ -51,8 +41,12 @@ ref_state = load_state(fname, variables)
 # current solution
 #
 fname = os.path.join(
-    "../runs/icon4py/test_channel_950m_x_350m_res5m_nlev20.updated",
-    "end_of_timestep_000000009.pkl",
+    #"../icon-exclaim/build_cpu2py/experiments/exclaim_gauss3d_sb/",
+    #"torus_insta_DOM01_ML_0002.nc",
+    "../icon-exclaim/build_cpu2py/experiments/exclaim_gauss3d_sb/undefined_output_runxxx/",
+    "000001_debug_after_diffu.pkl",
+    #"../icon-exclaim/build_cpu2py/experiments/exclaim_gauss3d_sb/undefined_output_runxxx/",
+    #"initial_condition_ibm.pkl"
 )
 cur_state = load_state(fname, variables)
 
@@ -60,6 +54,7 @@ cur_state = load_state(fname, variables)
 # compare
 #
 for variable in variables:
+    ref_shape = ref_state[variable].shape # needed for nproma in icon-exclaim
     print(
-        f"Variable: {variable} max abs diff: {abs(ref_state[variable] - cur_state[variable]).max()}"
+        f"Variable: {variable} max abs diff: {abs(ref_state[variable] - cur_state[variable][:ref_shape[0],:ref_shape[1]]).max()}"
     )
