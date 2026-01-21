a5f694e412e357a7bc6961376c90f0187156496d
Jacopo <jacopo.canton@gmail.com>
2026-01-21 11:58:50 +0100

    tmp

---
+++ domain_decomp_testing.py
@@ -0,0 +1,7 @@
+import xarray as xr
+
+
+grid = ux.open_grid(
+    "./testdata/grids/torus_1000x1000_res250/Torus_Triangles_1000m_x_1000m_res250m.nc"
+    #"./testdata/grids/r02b04_global/icon_grid_0013_R02B04_R.nc"
+)
